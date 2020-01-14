248a5f6992d0708ab5b9cbcaac147b5a1d20683e
ACEAkmoon <oleksandr.krushynskyi@whaleapp.com>
2020-01-14 21:23:04 +0200

    add pause

--- jobArchive7zipCurl.bat
+++ jobArchive7zipCurl.bat
@@ -4,6 +4,8 @@
 "C:\Program Files\7-Zip\"7z.exe a hopa-dev-0.1-%BUILD_NUMBER%-%BUILD_VCS_NUMBER:~0,7%.zip ".\target\hopa-0.1.jar"
 
 # ------------------- step curl push jenkins -------------------
-
 curl -X POST https://jenkins.aceakmoon.com/job/HOPA/job/DEV/job/deploy_hopa/build --user "USER:NAME":"USER:PASSWORD" --form file0=@hopa-dev-0.1-%BUILD_NUMBER%-%BUILD_VCS_NUMBER:~0,7%.zip --form json="{\"parameter\": [{\"name\":\"deploy_file\", \"file\":\"file0\"}]}"
+
+echo Done.
+pause
 
